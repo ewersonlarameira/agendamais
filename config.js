ca5c2e71d6fd8d5c7c5fd987f97a210f7af4a50f
ewersonlarameira <ewersonlarameira@gmail.com>
2026-09-20 22:24:36 -0300

    Rename supabase to supabaseClient in config.js
    Renamed supabase variable to supabaseClient to avoid conflicts.

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
-// config.js
 const SUPABASE_URL = 'https://xwtkfdfhmaaraodrusxx.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh3dGtmZGZobWFhcmFvZHJ1c3h4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk5NDgyNjIsImV4cCI6MjEwNTUyNDI2Mn0.2rpZVApvoN14cqL2Ny5KeFI2aXEtQI99MkVgYIQsfUw';
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+// Mudamos o nome para supabaseClient para evitar conflitos
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
